a52b842826e1076b59dbd6a4acf217d6c0870734
alshryhy1 <assan757@gmail.com>
2026-01-23 00:15:48 +0300

    Force Vercel Refresh

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // Force Vercel to ignore type errors during build
   typescript: {
     ignoreBuildErrors: true,
   },
